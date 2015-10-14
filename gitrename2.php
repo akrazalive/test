8e9f64895c56fcffe6fba2eb8caaea2fd2c3387f
Asif Khan <akrazalive@gmail.com>
2015-10-14 14:53:27 +0500

    =First Commit of First Branch

--- gitrename2.php
+++ gitrename2.php
@@ -1,16 +1,5 @@
 <?php
 
-
-echo "Hi there i am going ot "
-
-// First Git Commit for gitrename2
-echo "Asif Khan"
-
-echo "I am second Commit" ;
-
-echo "This is edited from Github"
-
-echo "This is from local and will go live on github in a while ."
-
+echo "Testing Branches Commit One" ;
 
 ?>
